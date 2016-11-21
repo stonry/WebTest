bdd85a3d6055395212bab37c853722dd9988b54c
WangXu <wangx@tuyouchina.com>
2016-11-21 10:35:23 +0800

    Initial Commit

---
+++ WebTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebTest
+//
+//  Created by 王旭 on 16/11/21.
+//  Copyright © 2016年 Stonry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebTest
+//
+//  Created by 王旭 on 16/11/21.
+//  Copyright © 2016年 Stonry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
